c177211853a5c7e9e7d0eb807418f30ab9bfafcf
Stilllee <orodae@gmail.com>
2024-11-27 16:00:46 +0900

    스크롤 & 다음 페이지 로딩 #2
    - api.js에 페이지 요청 메소드 추가
    - App.js에 페이지 삳태 관리 및 다음 페이지 로드 로직 구현
    - SearchResult에 무한 스크롤 이벤트 핸들러 추가

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -6,11 +6,14 @@ const api = {
       res.json()
     );
   },
-
+  fetchCatsPage: (keyword, page) => {
+    return fetch(
+      `${API_ENDPOINT}/api/cats/search?q=${keyword}&page=${page}`
+    ).then((res) => res.json());
+  },
   fetchRandomCats: () => {
     return fetch(`${API_ENDPOINT}/api/cats/random50`).then((res) => res.json());
   },
-
   fetchCatDetail: (id) => {
     return fetch(`${API_ENDPOINT}/api/cats/${id}`).then((res) => res.json());
   },
